30def681a4b40a3c54048d9710334c3508a85339
LeanneCreedon <D00236480@student.dkit.ie>
2024-05-20 17:44:21 +0100

    :sparkles: Bake Scene Lighting
    Add Occlusion Portal. Add Light Probes and Reflection Probes.

--- Assets/_ICAProject/ThirdPartyAssets/Scripts/Occlusion/OcclusionPortalScript.cs
+++ Assets/_ICAProject/ThirdPartyAssets/Scripts/Occlusion/OcclusionPortalScript.cs
@@ -1,26 +1,107 @@
-using UnityEngine;
+/*using UnityEngine;
 
 public class OcclusionPortalScript : MonoBehaviour
 {
     public OcclusionPortal myOcclusionPortal;
-    public Animator animator;
-    public bool doorOpen = false;
-
-    void OpenPortal()
-    {
-        // Toggle the Occlusion Portal's open state, so that Unity renders the GameObjects behind it
-        myOcclusionPortal.open = true;
-    }
+    public GameObject door;
+    public BoxCollider doorCollider;
+    public GameObject ui;
+    private bool isPlayerInZone = false;
+    private bool isPortalOpen = false;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            Debug.Log("In Trigger");
-            OpenPortal();
-            animator.Play("DoorOpen");
-            doorOpen = true;
+            ui.SetActive(true);
+            isPlayerInZone = true;
         }
     }
 
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ui.SetActive(false);
+            isPlayerInZone = false;
+        }
+    }
+
+    void Update()
+    {
+        if (isPlayerInZone && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("E key pressed while in the trigger zone.");
+            TogglePortal();
+        }
+    }
+
+    private void TogglePortal()
+    {
+
+        isPortalOpen = !isPortalOpen;
+        myOcclusionPortal.open = isPortalOpen;
+
+        doorCollider.enabled = !isPortalOpen;
+        door.GetComponent<MeshRenderer>().enabled = !isPortalOpen;
+
+        if (isPortalOpen)
+        {
+            Debug.Log("Portal opened, room objects should be visible.");
+        }
+        else
+        {
+            Debug.Log("Portal closed, room objects should be occluded.");
+        }
+    }
+}
+*/
+
+using UnityEngine;
+
+public class OcclusionPortalScript : MonoBehaviour
+{
+    public OcclusionPortal myOcclusionPortal;
+    public GameObject door;
+    public BoxCollider doorCollider;
+    public GameObject ui;
+    private bool isPlayerInZone = false;
+    private bool isPortalOpen = false;
+    private bool isDoorColliderOpen = true;
+    private bool isMeshRendererOpen = true;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ui.SetActive(true);
+            isPlayerInZone = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ui.SetActive(false);
+            isPlayerInZone = false;
+        }
+    }
+
+
+    void Update()
+    {
+        if (isPlayerInZone && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("E key pressed while in the trigger zone.");
+            isPortalOpen = !isPortalOpen;
+            myOcclusionPortal.open = isPortalOpen;
+
+            isDoorColliderOpen = !isDoorColliderOpen;
+            doorCollider.GetComponent<BoxCollider>().enabled = isDoorColliderOpen;
+
+            isMeshRendererOpen = !isMeshRendererOpen;
+            door.GetComponent<MeshRenderer>().enabled = isMeshRendererOpen;
+        }
+    }
 }
